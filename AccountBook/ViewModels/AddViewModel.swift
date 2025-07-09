471692c7d50cff9a7b41633f92e76c44d921f0eb
CoCoral73 <garden0073@gmail.com>
2025-07-09 18:32:21 +0900

    폴더 정리

---
+++ AccountBook/ViewModels/AddViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 7/9/25.
+//
+
+import UIKit
+
+class AddViewModel {
+    
+}
---
+++ AccountBook/Views/AddViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 7/9/25.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
