c059cee88f67c3f7d14c56589280c7295783e7e4
CoCoral73 <garden0073@gmail.com>
2025-09-19 18:46:10 +0900

    차트 뷰 초기 세팅

---
+++ AccountBook/ViewModels/ChartViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ChartViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 9/19/25.
+//
+
+import UIKit
+
+class ChartViewModel {
+    
+}
---
+++ AccountBook/Views/ChartViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChartViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 9/19/25.
+//
+
+import UIKit
+import Charts
+
+class ChartViewController: UIViewController {
+
+    @IBOutlet weak var periodButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func periodButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
