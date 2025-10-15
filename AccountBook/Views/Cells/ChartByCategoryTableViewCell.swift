dd72231ec0c5fdb638c876b2220bd5c618c85a3f
CoCoral73 <garden0073@gmail.com>
2025-10-15 12:30:05 +0900

    셀클래스 폴더 이동

---
+++ AccountBook/Views/Cells/ChartByCategoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChartByCategoryTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 10/14/25.
+//
+
+import UIKit
+
+class ChartByCategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
