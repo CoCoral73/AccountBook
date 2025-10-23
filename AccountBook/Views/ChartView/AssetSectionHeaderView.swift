7c4a07cc0169fdaa7346dc253991eab3da136d3a
CoCoral73 <garden0073@gmail.com>
2025-10-23 15:57:17 +0900

    자산 테이블 헤더뷰 UI 바인딩

--- AccountBook/Views/ChartView/AssetSectionHeaderView.swift
+++ AccountBook/Views/ChartView/AssetSectionHeaderView.swift
@@ -10,13 +10,15 @@ import UIKit
 class AssetSectionHeaderView: UITableViewHeaderFooterView {
     @IBOutlet weak var pointBar: UIView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratioLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     
     private let color: [UIColor] = [chartColors[0], chartColors[2], chartColors[3], chartColors[4]]
     
-    func configure(_ section: Int, _ name: String, _ amount: String) {
+    func configure(_ section: Int, _ name: String, _ ratio: String, _ amount: String) {
         pointBar.backgroundColor = color[section]
         nameLabel.text = name
+        ratioLabel.text = ratio
         amountLabel.text = amount
     }
 }
