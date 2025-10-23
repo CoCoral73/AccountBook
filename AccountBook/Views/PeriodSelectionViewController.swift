4a711eea24b0444964a556247bc7d0fffbc0c013
CoCoral73 <garden0073@gmail.com>
2025-10-23 18:03:41 +0900

    기간 선택 뷰, 컨트롤러 생성

---
+++ AccountBook/Views/PeriodSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PeriodSelectionViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 10/23/25.
+//
+
+import UIKit
+
+class PeriodSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
