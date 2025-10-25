445dea3ae6a1fbbe1aec8388e4dc1343c222844d
CoCoral73 <garden0073@gmail.com>
2025-10-25 16:27:33 +0900

    앱 기본값 구조체 및 날짜 상수 선언

---
+++ AccountBook/Models/DefaultSetting.swift
@@ -0,0 +1,11 @@
+//
+//  Untitled.swift
+//  AccountBook
+//
+//  Created by 김정원 on 10/25/25.
+//
+
+struct DefaultSetting {
+    static let year: [Int] = Array(2020...2100)
+    static let month: [Int] = Array(1...12)
+}
