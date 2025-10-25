7873a88cba09b669b54b51d116dc7195863b2aa7
CoCoral73 <garden0073@gmail.com>
2025-10-25 15:44:56 +0900

    기간 선택 뷰 UI 구성

--- AccountBook/Views/PeriodSelectionViewController.swift
+++ AccountBook/Views/PeriodSelectionViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class PeriodSelectionViewController: UIViewController {
 
+    @IBOutlet weak var borderView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        borderView.layer.cornerRadius = 20
+        borderView.layer.borderWidth = 2
+        borderView.layer.borderColor = UIColor.lightGray.cgColor
     }
-    */
-
 }
