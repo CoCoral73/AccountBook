e132d34bd6a4dcb78a08a01faffed4e80e09b705
CoCoral73 <garden0073@gmail.com>
2025-10-25 20:46:05 +0900

    데이터 바인딩

--- AccountBook/Views/PeriodSelectionViewController.swift
+++ AccountBook/Views/PeriodSelectionViewController.swift
@@ -16,16 +16,77 @@ class PeriodSelectionViewController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var pickerView: UIPickerView!
     
+    var viewModel: PeriodSelectionViewModel
+    
+    required init?(coder: NSCoder, viewModel: PeriodSelectionViewModel) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        viewModel.onDidChangedDatePicker = { [weak self] in
+            guard let self = self else { return }
+            startDateButton.setTitle(viewModel.startDateButtonString, for: .selected)
+            endDateButton.setTitle(viewModel.endDateButtonString, for: .selected)
+        }
+        
+        configurePickerView()
+        bindViewModel()
+    }
+    
+    func configurePickerView() {
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        pickerView.selectRow(viewModel.selectedRowForYear, inComponent: 0, animated: false)
+        pickerView.selectRow(viewModel.selectedRowForMonth, inComponent: 1, animated: false)
+    }
+    
+    func bindViewModel() {
+        startDateButton.setTitle(viewModel.startDateButtonString, for: .normal)
+        endDateButton.setTitle(viewModel.endDateButtonString, for: .normal)
+        startDateButton.isEnabled = viewModel.dateButtonEnabled
+        endDateButton.isEnabled = viewModel.dateButtonEnabled
+        
+        pickerView.isHidden = viewModel.isHiddenForPickerView
+        datePicker.isHidden = viewModel.isHiddenForDatePicker
+        
+        pickerView.selectRow(viewModel.selectedRowForYear, inComponent: 0, animated: false)
+        pickerView.selectRow(viewModel.selectedRowForMonth, inComponent: 1, animated: false)
     }
     
     @IBAction func segControlChanged(_ sender: UISegmentedControl) {
+        viewModel.setPeriodType(sender.selectedSegmentIndex)
+        bindViewModel()
     }
+    
     @IBAction func dateButtonTapped(_ sender: UIButton) {
+        viewModel.handleDateButton(sender.tag)
+        startDateButton.isSelected = startDateButton.tag == sender.tag
+        endDateButton.isSelected = endDateButton.tag == sender.tag
     }
+    
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        viewModel.handleDatePicker(sender.date)
     }
+extension PeriodSelectionViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == 0 ? viewModel.years.count : viewModel.months.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return component == 0 ? "\(viewModel.years[row])년" : "\(viewModel.months[row])월"
+    }
+    
 }
