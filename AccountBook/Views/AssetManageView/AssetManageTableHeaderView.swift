4b50550eeb73111cfde1bd837f29556c59fb19b8
CoCoral73 <garden0073@gmail.com>
2025-11-05 15:29:10 +0900

    헤더뷰 클래스 연결

---
+++ AccountBook/Views/AssetManageView/AssetManageTableHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  AssetManageTableHeaderView.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/5/25.
+//
+
+import UIKit
+
+class AssetManageTableHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+}
