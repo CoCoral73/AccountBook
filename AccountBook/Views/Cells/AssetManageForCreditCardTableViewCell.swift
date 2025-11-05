00a8e786bddb48c375f86d3280ae9a285acfc9fd
CoCoral73 <garden0073@gmail.com>
2025-11-05 14:13:42 +0900

    테이블뷰 셀클래스 연결

---
+++ AccountBook/Views/Cells/AssetManageForCreditCardTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AssetManageForCreditCardTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/5/25.
+//
+
+import UIKit
+
+class AssetManageForCreditCardTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var currentMonthSpendingLabel: UILabel!
+    @IBOutlet weak var upcomingPaymentAmountLabel: UILabel!
+}
---
+++ AccountBook/Views/Cells/AssetManageTableViewForNotCreditCardCell.swift
@@ -0,0 +1,15 @@
+//
+//  AssetManageTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/5/25.
+//
+
+import UIKit
+
+class AssetManageTableViewForNotCreditCardCell: UITableViewCell {
+
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+}
