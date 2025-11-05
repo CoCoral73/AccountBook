5f0eb600dc62a041191b5f18db509c8c39b22b96
CoCoral73 <garden0073@gmail.com>
2025-11-05 12:20:11 +0900

    자산 관리 뷰 추가

---
+++ AccountBook/Views/AssetManageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AssetManageViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/5/25.
+//
+
+import UIKit
+
+class AssetManageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
