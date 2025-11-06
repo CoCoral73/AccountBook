efb969e136b577ebf0b4e57c74f2e5152150ed11
CoCoral73 <garden0073@gmail.com>
2025-11-06 16:58:37 +0900

    뷰모델 잔액 데이터 가공

--- AccountBook/ViewModels/AssetManageView/AssetManageViewModel.swift
+++ AccountBook/ViewModels/AssetManageView/AssetManageViewModel.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class AssetManageViewModel {
     
+    var assetTotalAmountString: String {
+        return totalBalance.formattedWithComma + "원"
+    }
+    
+    var totalBalance: Int64 {
+        return AssetItemManager.shared.cash[0].balance + bankBalance
+    }
+    
+    var bankBalance: Int64 {
+        return AssetItemManager.shared.bankAccount.reduce(0) { $0 + $1.balance }
+    }
+    
     func numberOfRowsInSection(section: Int) -> Int {
         guard let type = AssetType(rawValue: section) else {
             print("AssetManageViewModel: AssetType 생성 오류")
@@ -36,14 +48,11 @@ class AssetManageViewModel {
         
         let name = type.displayName
         if section == 1 {
-            let amount = calculateTotalBalance().formattedWithComma + "원"
+            let amount = bankBalance.formattedWithComma + "원"
             return (name, amount)
         } else {
             return (name, "")
         }
     }
     
-    func calculateTotalBalance() -> Int64 {
-        return AssetItemManager.shared.bankAccount.reduce(0) { $0 + $1.balance }
-    }
 }
