91d76bb7788419f94db07daac05c60ab34244055
CoCoral73 <garden0073@gmail.com>
2025-11-06 15:35:06 +0900

    뷰컨 테이블뷰 바인딩

--- AccountBook/Views/AssetManageView/AssetManageViewController.swift
+++ AccountBook/Views/AssetManageView/AssetManageViewController.swift
@@ -13,11 +13,22 @@ class AssetManageViewController: UIViewController {
     @IBOutlet weak var tableView: IntrinsicTableView!
     
     var viewModel: AssetManageViewModel!
+    var tableViewHandler: AssetManageTableViewHandler!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableViewHandler = AssetManageTableViewHandler(viewModel: viewModel)
+        tableView.dataSource = tableViewHandler
+        tableView.delegate = tableViewHandler
+        tableView.sectionHeaderTopPadding = 15
         
+        let headerNib = UINib(nibName: "AssetManageTableHeaderView", bundle: nil)
+        tableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "AssetManageTableHeaderView")
     }
 
     @IBAction func infoButtonTapped(_ sender: UIButton) {
