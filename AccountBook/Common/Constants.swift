44ebeff58cf0fdb706e440a2ddd23b74e2af6faf
CoCoral73 <garden0073@gmail.com>
2025-11-12 15:35:00 +0900

    셀 id 상수 선언

--- AccountBook/Common/Constants.swift
+++ AccountBook/Common/Constants.swift
@@ -14,4 +14,5 @@ struct Cell {
     static let tableByAssetCellInChart = "TableByAssetInChart"
     static let assetManageForNotCreditCardCell = "AssetManageForNotCreditCardCell"
     static let assetManageForCreditCardCell = "AssetManageForCreditCardCell"
+    static let linkedCardsCell = "LinkedCardsCell"
 }
