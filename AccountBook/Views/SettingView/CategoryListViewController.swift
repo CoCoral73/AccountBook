1592a5a8d108b9182fd8a8039aec3225d7863dab
CoCoral73 <garden0073@gmail.com>
2025-11-12 19:00:35 +0900

    카테고리 리스트뷰: 뷰컨트롤러 생성

---
+++ AccountBook/Views/SettingView/CategoryListViewController.swift
@@ -0,0 +1,83 @@
+//
+//  CategoryListViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/12/25.
+//
+
+import UIKit
+
+class CategoryListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var viewModel: CategoryListViewModel
+    
+    required init?(coder: NSCoder, viewModel: CategoryListViewModel) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureTableView()
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        let vm = CategoryAddViewModel(isIncome: viewModel.isIncome)
+        vm.onDidAddCategory = {
+            self.tableView.reloadData()
+        }
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "CategoryAddViewController", creator: { coder in
+            CategoryAddViewController(coder: coder, viewModel: vm)
+        }) else {
+            print("CategoryListViewController: VC 생성 오류")
+            return
+        }
+        vc.presentationStyle = .push
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
+
+extension CategoryListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryListCell", for: indexPath) as! CategoryListTableViewCell
+        cell.model = viewModel.cellForRowAt(row: indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool { true }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        viewModel.moveRowAt(source: sourceIndexPath.row, destination: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+}
