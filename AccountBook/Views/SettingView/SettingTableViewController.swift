e48b1731c8b9496a65508904a1edb76256fadb6e
CoCoral73 <garden0073@gmail.com>
2025-11-12 18:55:10 +0900

    설정탭 테이블뷰컨트롤러 생성

---
+++ AccountBook/Views/SettingView/SettingTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingTableViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/12/25.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
