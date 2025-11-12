5a15ff055fc42a61fa5de814ee260ad9d71db93d
CoCoral73 <garden0073@gmail.com>
2025-11-12 18:55:38 +0900

    설정탭 열거형 타입 정의

--- AccountBook/Views/SettingView/SettingTableViewController.swift
+++ AccountBook/Views/SettingView/SettingTableViewController.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
+enum SettingOption {
+    case incomeCategory, expenseCategory
+    case theme, lockApp
+    case help, contact, rating
+    
+    init(indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            self = .incomeCategory
+        case (0, 1):
+            self = .expenseCategory
+        case (1, 0):
+            self = .theme
+        case (1, 1):
+            self = .lockApp
+        case (2, 0):
+            self = .help
+        case (2, 1):
+            self = .contact
+        default: //(2, 2)
+            self = .rating
+        }
+    }
+}
+
 class SettingTableViewController: UITableViewController {
 
     override func viewDidLoad() {
