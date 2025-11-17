f49b5303b45b2c51d8c7f1d49fec54caf59c979f
CoCoral73 <garden0073@gmail.com>
2025-11-17 14:08:31 +0900

    feat(AppTheme): define value of common property

--- AccountBook/Common/AppTheme/AppTheme.swift
+++ AccountBook/Common/AppTheme/AppTheme.swift
@@ -26,3 +26,23 @@ protocol AppTheme {
     
     var chartColors: [UIColor] { get }
 }
+
+extension AppTheme {
+    var primaryTextColor: UIColor { .label }
+    var secondaryTextColor: UIColor { .secondaryLabel }
+    var chartColors: [UIColor] {
+        [
+            //빨 주 노 초 하 파 보 쿨핑 핑 회
+            UIColor(red: 1.00, green: 0.64, blue: 0.64, alpha: 1.00),
+            UIColor(red: 1.00, green: 0.75, blue: 0.64, alpha: 1.00),
+            UIColor(red: 1.00, green: 0.88, blue: 0.64, alpha: 1.00),
+            UIColor(red: 0.77, green: 1.00, blue: 0.64, alpha: 1.00),
+            UIColor(red: 0.64, green: 1.00, blue: 0.99, alpha: 1.00),
+            UIColor(red: 0.64, green: 0.84, blue: 1.00, alpha: 1.00),
+            UIColor(red: 0.80, green: 0.64, blue: 1.00, alpha: 1.00),
+            UIColor(red: 1.00, green: 0.68, blue: 0.96, alpha: 1.00),
+            UIColor(red: 1.00, green: 0.78, blue: 0.78, alpha: 1.00),
+            UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.00)
+        ]
+    }
+}
