b8f4ec380717cfc2f3ea96997985fcf0d42cb058
CoCoral73 <garden0073@gmail.com>
2025-11-17 14:51:46 +0900

    feat: define ThemeApplicable protocol

---
+++ AccountBook/Common/AppTheme/ThemeApplicable.swift
@@ -0,0 +1,36 @@
+//
+//  ThemeApplicable.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/17/25.
+//
+import UIKit
+
+protocol ThemeApplicable: AnyObject {
+    func applyTheme(_ theme: AppTheme)
+}
+
+extension ThemeApplicable where Self: UIViewController {
+
+    func startObservingTheme() {
+        NotificationCenter.default.addObserver(
+            forName: .themeDidChange,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            guard let self else { return }
+            self.applyTheme(ThemeManager.shared.currentTheme)
+        }
+    }
+
+    func stopObservingTheme() {
+        NotificationCenter.default.removeObserver(self,
+            name: .themeDidChange,
+            object: nil)
+    }
+
+    /// 처음 화면 등장 시 테마 초기 적용
+    func applyInitialTheme() {
+        applyTheme(ThemeManager.shared.currentTheme)
+    }
+}
