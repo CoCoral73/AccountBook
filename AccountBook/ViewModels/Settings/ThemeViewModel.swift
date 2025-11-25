4ca382392181c937aa783c0a1f8690b2b6959479
CoCoral73 <garden0073@gmail.com>
2025-11-25 18:55:15 +0900

    feat(Theme): initial configuration for Theme View

---
+++ AccountBook/ViewModels/Settings/ThemeViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ThemeViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/25/25.
+//
+
+class ThemeViewModel {
+    
+}
---
+++ AccountBook/Views/SettingView/ThemeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 11/25/25.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
