b3b47a326b7d0b59b980865ae0874054e790bd93
CoCoral73 <garden0073@gmail.com>
2025-12-01 17:50:42 +0900

    feat(LockAppVC): configure UI and connect it to VC

--- AccountBook/Views/SettingView/LockAppViewController.swift
+++ AccountBook/Views/SettingView/LockAppViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class LockAppViewController: UIViewController {
 
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var lockSwitch: UISwitch!
+    @IBOutlet weak var detailViewForLockState: UIStackView!
+    @IBOutlet weak var modifyPWView: UIView!
+    @IBOutlet weak var faceIDSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +29,9 @@ class LockAppViewController: UIViewController {
 
     @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
+    }
+    @IBAction func lockSwitchChanged(_ sender: UISwitch) {
+    }
+    @IBAction func faceIDSwitchChanged(_ sender: UISwitch) {
     }
 }
