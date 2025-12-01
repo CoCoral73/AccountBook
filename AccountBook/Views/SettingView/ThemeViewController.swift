f4033870e1ccf13d19a8137b8be5bfb38f4c8722
CoCoral73 <garden0073@gmail.com>
2025-12-01 16:00:34 +0900

    feat(ThemeVC): apply view model's data

--- AccountBook/Views/SettingView/ThemeViewController.swift
+++ AccountBook/Views/SettingView/ThemeViewController.swift
@@ -14,11 +14,13 @@ class ThemeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var viewModel = ThemeViewModel()
+    var flag = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar()
+        configureTableView()
     }
     
     func configureNavigationBar() {
@@ -27,8 +29,39 @@ class ThemeViewController: UIViewController {
         navItem.standardAppearance = appearance
     }
 
+    func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
     @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if flag { return }
+        
+        let indexPath = IndexPath(row: viewModel.selectedRow, section: 0)
+        tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        flag = true
+    }
+}
+
+extension ThemeViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cell.themeCell, for: indexPath) as! ThemeTableViewCell
+        cell.model = viewModel.cellForRowAt(row: indexPath.row)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.didSelectRowAt(row: indexPath.row)
+    }
 }
