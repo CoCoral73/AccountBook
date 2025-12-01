fe96c5f58d695be1d6aaf97765410e1ed1a5d28e
CoCoral73 <garden0073@gmail.com>
2025-12-01 15:59:42 +0900

    feat(ThemeCell): define Theme Cell class

---
+++ AccountBook/Views/Cells/ThemeTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 12/1/25.
+//
+
+import UIKit
+
+class ThemeTableViewCell: UITableViewCell {
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var checkView: UIImageView!
+    
+    var model: AppTheme! {
+        didSet {
+            configure()
+        }
+    }
+    
+    func configure() {
+        colorView.backgroundColor = model.accentColor
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        checkView.isHidden = !selected
+    }
+}
