78fb3c10c2a24aaf810ec2a633a3acb537a60345
CoCoral73 <garden0073@gmail.com>
2025-12-01 15:58:34 +0900

    feat(ThemeVM): make data for VC

--- AccountBook/ViewModels/Settings/ThemeViewModel.swift
+++ AccountBook/ViewModels/Settings/ThemeViewModel.swift
@@ -6,5 +6,22 @@
 //
 
 class ThemeViewModel {
+    let manager = ThemeManager.shared
     
+    var selectedRow: Int {
+        return manager.listOfThemes.firstIndex(of: manager.currentKind)!
+    }
+    
+    var numberOfRowsInSection: Int {
+        return manager.countOfThemes
+    }
+    
+    func cellForRowAt(row: Int) -> AppTheme {
+        return manager.listOfThemes[row].theme
+    }
+    
+    func didSelectRowAt(row: Int) {
+        let kind = manager.listOfThemes[row]
+        manager.apply(kind: kind)
+    }
 }
