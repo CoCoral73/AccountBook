7fa761f51fdfa905d5b9a662ce6db32b4240c525
CoCoral73 <garden0073@gmail.com>
2025-12-01 13:42:27 +0900

    feat(ThemeVC): define custom initializer for setting view model

--- AccountBook/Views/SettingView/ThemeViewController.swift
+++ AccountBook/Views/SettingView/ThemeViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ThemeViewController: UIViewController {
 
+    var viewModel: ThemeViewModel!
+    
+    required init?(coder: NSCoder, viewModel: ThemeViewModel) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
