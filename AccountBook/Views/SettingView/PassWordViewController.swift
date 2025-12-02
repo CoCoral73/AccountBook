fa5128611e7efdae5683d015d0b3bdc7f0f319e7
CoCoral73 <garden0073@gmail.com>
2025-12-02 16:10:02 +0900

    feat(PWVC): connect UI to VC

--- AccountBook/Views/SettingView/PassWordViewController.swift
+++ AccountBook/Views/SettingView/PassWordViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class PassWordViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var pwView: UIStackView!
+    @IBOutlet weak var pw1Label: UILabel!
+    @IBOutlet weak var pw2Label: UILabel!
+    @IBOutlet weak var pw3Label: UILabel!
+    @IBOutlet weak var pw4Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberPadTapped(_ sender: UIButton) {
+    }
+    
 }
