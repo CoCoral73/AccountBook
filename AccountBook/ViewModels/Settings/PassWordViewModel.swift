aa69cba3dde8d247829b01f7e66f50ceb6745d4f
CoCoral73 <garden0073@gmail.com>
2025-12-02 15:14:42 +0900

    feat: initial configuration for PassWord View

---
+++ AccountBook/ViewModels/Settings/PassWordViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  PassWordViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 12/2/25.
+//
+
+class PassWordViewModel {
+    
+}
---
+++ AccountBook/Views/SettingView/PassWordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PassWordViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 12/2/25.
+//
+
+import UIKit
+
+class PassWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
