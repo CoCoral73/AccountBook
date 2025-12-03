a0a6c94d39055f7fa0408a5e011e0f51b9e590e2
CoCoral73 <garden0073@gmail.com>
2025-12-03 16:01:09 +0900

    feat(LockAppVM): make data and method for VC

--- AccountBook/ViewModels/Settings/LockAppViewModel.swift
+++ AccountBook/ViewModels/Settings/LockAppViewModel.swift
@@ -6,5 +6,47 @@
 //
 
 class LockAppViewModel {
+    var isLocked: Bool {
+        didSet {
+            onDidUpdateLockState?()
+        }
+    }
     
+    var onDidUpdateLockState: (() -> Void)?
+    var onRequestShowPassword: ((PasswordViewModel) -> Void)?
+    
+    init() {
+        isLocked = LockAppManager.shared.isLocked
+    }
+    
+    var isOnForLockSwitch: Bool { isLocked }
+    var isHiddenForDetailView: Bool { !isLocked }
+    
+    func handleModifyPW() {
+        let vm = makePasswordVM(mode: .modify)
+        onRequestShowPassword?(vm)
+    }
+    
+    func handleLockSwitch(_ isOn: Bool) {
+        if isOn {
+            let vm = makePasswordVM(mode: .register)
+            onRequestShowPassword?(vm)
+        } else {
+            LockAppManager.shared.deletePassword()
+            isLocked = false
+        }
+    }
+    
+    func makePasswordVM(mode: PasswordMode) -> PasswordViewModel {
+        let vm = PasswordViewModel(mode: mode)
+        
+        vm.onDidFinish = { [weak self] state in
+            guard let self = self else { return }
+            if mode == .register {
+                self.isLocked = state
+            }
+        }
+        
+        return vm
+    }
 }
