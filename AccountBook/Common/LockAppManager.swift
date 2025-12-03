c0cc557bd25e3302fd5a9e7d6a940a073fdda661
CoCoral73 <garden0073@gmail.com>
2025-12-03 15:59:31 +0900

    feat(LockAppManager): implement handle password logic temporarily

--- AccountBook/Common/LockAppManager.swift
+++ AccountBook/Common/LockAppManager.swift
@@ -9,5 +9,21 @@ class LockAppManager {
     static let shared = LockAppManager()
     private init() { }
     
+    private var password: String?
     
+    var isLocked: Bool { password != nil }
+    
+    func registerPassword(_ pw: [Int]) {
+        let pw = pw.map { String($0) }.joined()
+        password = pw
+    }
+    
+    func validatePassword(_ pw: [Int]) -> Bool {
+        let pw = pw.map { String($0) }.joined()
+        return pw == password
+    }
+    
+    func deletePassword() {
+        password = nil
+    }
 }
