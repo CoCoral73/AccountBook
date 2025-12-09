05eff2c8dd21f36b13d8bab23212f57ea8d6941b
CoCoral73 <garden0073@gmail.com>
2025-12-09 15:12:56 +0900

    feat(LockAppManager): implement clearing keychain data when first launch

--- AccountBook/Common/LockAppManager.swift
+++ AccountBook/Common/LockAppManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum LockAppKey {
+    static let hasLaunchedBefore = "lockapp.hasLaunchedBefore"
     static let isLocked = "lockapp.isLocked"                //UserDefaults
     static let passwordKey = "lockapp.password"             //Keychain
     static let permissionState = "lockapp.biometric.permission.state" //UserDefaults
@@ -36,6 +37,21 @@ class LockAppManager {
     var isLocked: Bool { UserDefaults.standard.isLocked }
     var permissionState: BiometricPermissionState { UserDefaults.standard.permissionState }
     var useBiometricID: Bool { UserDefaults.standard.useBiometricID }
+    
+    func clearKeychainDataIfFirstLaunch() {
+        if UserDefaults.standard.bool(forKey: LockAppKey.hasLaunchedBefore) == false {
+            let success = KeychainManager.shared.delete(forKey: LockAppKey.passwordKey)
+            
+            switch success {
+            case true:
+                print("Clear Keychain Data: Success")
+            case false:
+                print("Clear Keychain Data: Fail")
+            }
+            
+            UserDefaults.standard.set(true, forKey: LockAppKey.hasLaunchedBefore)
+        }
+    }
     
     func registerPassword(_ pw: [Int]) {
         let pw = pw.map { String($0) }.joined()
