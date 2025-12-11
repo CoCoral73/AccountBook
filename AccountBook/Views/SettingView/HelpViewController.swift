992f60babd7db9ef7d2b62779c03edf9094fc00c
CoCoral73 <garden0073@gmail.com>
2025-12-11 16:06:47 +0900

    feat(Help): configure UI for navigation bar

--- AccountBook/Views/SettingView/HelpViewController.swift
+++ AccountBook/Views/SettingView/HelpViewController.swift
@@ -10,15 +10,22 @@ import WebKit
 
 class HelpViewController: UIViewController {
 
+    @IBOutlet weak var navItem: UINavigationItem!
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configure()
+        configureNavigationBar()
+        configureURL()
     }
 
-    private func configure() {
+    private func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        navItem.standardAppearance = appearance
+    }
+    private func configureURL() {
         let url = URL(string: "https://instinctive-pixie-038.notion.site/2c6a5a562f278087bb76cc207547cbde?pvs=74")!
         let request = URLRequest(url: url)
         webView.load(request)
