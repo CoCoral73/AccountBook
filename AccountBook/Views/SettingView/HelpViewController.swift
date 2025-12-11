e70bd010ec3b2c0e798ccd9da95cdffd186b556b
CoCoral73 <garden0073@gmail.com>
2025-12-11 15:54:13 +0900

    feat: initial configuration for Help view

---
+++ AccountBook/Views/SettingView/HelpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HelpViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 12/11/25.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
