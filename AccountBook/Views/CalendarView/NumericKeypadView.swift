5e41cf759a56cdafda9fbc432fd0899ad184cb6f
CoCoral73 <garden0073@gmail.com>
2025-12-16 22:12:26 +0900

    feat: define protocol [NumericKeypadDelegate]

--- AccountBook/Views/CalendarView/NumericKeypadView.swift
+++ AccountBook/Views/CalendarView/NumericKeypadView.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+protocol NumericKeypadDelegate: AnyObject {
+    func keypadDidInput(number: Int)
+    func keypadDidOperator(op: Operator)
+    func keypadDidDelete()
+    func keypadDidAllClear()
+    func keypadDidHide()
+}
+
 class NumericKeypadView: UIView {
     static func loadFromNib() -> NumericKeypadView {
         return UINib(nibName: "NumericKeypadView", bundle: nil).instantiate(withOwner: nil).first as! NumericKeypadView
