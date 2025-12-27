021d64407c085be1c6590f7f133e88e552b68e41
CoCoral73 <garden0073@gmail.com>
2025-12-27 21:16:02 +0900

    feat: create ViewController and Storyboard view for [AssetItemDetailView]

---
+++ AccountBook/Views/AssetManageView/AssetItemDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AssetItemDetailViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 12/27/25.
+//
+
+import UIKit
+
+class AssetItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
