50f7df65568437f44be83bd33d972751c29a84c0
CoCoral73 <garden0073@gmail.com>
2025-12-27 22:27:34 +0900

    feat: add ViewModel property and implement custom initializer for [AssetItemDetailViewController]

--- AccountBook/Views/AssetManageView/AssetItemDetailViewController.swift
+++ AccountBook/Views/AssetManageView/AssetItemDetailViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class AssetItemDetailViewController: UIViewController {
 
     @IBOutlet weak var navItem: UINavigationItem!
+    
+    var viewModel: AssetItemDetailViewModel
+    
+    required init?(coder: NSCoder, viewModel: AssetItemDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
