75edc3a5690f4929cfbac2539ddd165c3de39563
CoCoral73 <garden0073@gmail.com>
2026-01-08 00:02:27 +0900

    feat: create TransactionHistoryTableViewCell class and connet IBOutlets

---
+++ AccountBook/Views/AssetManageView/TransactionHistoryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TransactionHistoryTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 1/7/26.
+//
+
+import UIKit
+
+class TransactionHistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var typeView: UIView!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
