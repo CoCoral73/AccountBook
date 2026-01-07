4c37ba1558c730e073970b9cfab1b3cff6721d62
CoCoral73 <garden0073@gmail.com>
2026-01-08 00:03:09 +0900

    feat: create HistoryCellViewModel and implement UI data processing

---
+++ AccountBook/ViewModels/AssetManageView/HistoryCellViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  HistoryCellViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 1/7/26.
+//
+
+import Foundation
+
+class HistoryCellViewModel {
+    var transaction: Transaction
+    var asset: AssetItem
+    
+    init(transaction: Transaction, asset: AssetItem) {
+        self.transaction = transaction
+        self.asset = asset
+    }
+    
+    var typeName: String {
+        return transaction.type.name
+    }
+    var name: String {
+        if transaction.type != .transfer { return transaction.name }
+        
+        let suffix: String
+        if transaction.fromAccount! == asset as! BankAccountItem {
+            suffix = "(→ \(transaction.toAccount!.name))"
+        } else {
+            suffix = "(\(transaction.fromAccount!.name) →)"
+        }
+        return [transaction.name, suffix].filter { !$0.isEmpty }.joined(separator: " ")
+    }
+    var categoryName: String {
+        return transaction.category.name
+    }
+    var amountDisplay: String {
+        let amountString = transaction.amount.formattedWithComma
+        switch transaction.type {
+        case .income:
+            return "+" + amountString
+        case .expense:
+            return "-" + amountString
+        case .transfer:
+            if transaction.fromAccount! == asset as! BankAccountItem {
+                return "-" + amountString
+            } else {
+                return "+" + amountString
+            }
+        }
+    }
+    var dateDisplay: String {
+        let fmt = DateFormatter()
+        fmt.dateFormat = "yyyy.MM.dd HH:mm:ss"
+        return fmt.string(from: transaction.date)
+    }
+}
