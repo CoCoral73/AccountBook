4242598f044d52a86b9f997b8b22503d140ffcbd
CoCoral73 <garden0073@gmail.com>
2026-01-12 15:30:16 +0900

    feat: configure custom navigation bar and apply ThemeApplicable protocol

--- AccountBook/Views/CalendarView/SearchViewController.swift
+++ AccountBook/Views/CalendarView/SearchViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, ThemeApplicable {
 
+    @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    
     var viewModel: SearchViewModel
     
     required init?(coder: NSCoder, viewModel: SearchViewModel) {
@@ -23,7 +26,30 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        startObservingTheme()
+        configureNavigationBar()
     }
 
+    func applyTheme(_ theme: any AppTheme) {
+        backButton.tintColor = theme.accentColor
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        navItem.standardAppearance = appearance
+    }
+    
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        applyInitialTheme()
+    }
+    
+    deinit {
+        stopObservingTheme()
+    }
 }
