3951279386d0704ff370ff48eaf89e87645325b2
CoCoral73 <garden0073@gmail.com>
2026-01-12 13:26:13 +0900

    feat: create skeleton files and storyboard scene for search feature

---
+++ AccountBook/ViewModels/CalendarView/SearchViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SearchViewModel.swift
+//  AccountBook
+//
+//  Created by 김정원 on 1/12/26.
+//
+
+class SearchViewModel {
+    
+}
---
+++ AccountBook/Views/CalendarView/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  AccountBook
+//
+//  Created by 김정원 on 1/12/26.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
