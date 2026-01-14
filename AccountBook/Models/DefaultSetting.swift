cb5960f7948d43b9856b472d6068d418f05bb51d
CoCoral73 <garden0073@gmail.com>
2026-01-14 16:48:52 +0900

    feat: declare firstDate and lastDate in DefaultSetting

--- AccountBook/Models/DefaultSetting.swift
+++ AccountBook/Models/DefaultSetting.swift
@@ -5,7 +5,11 @@
 //  Created by 김정원 on 10/25/25.
 //
 
+import Foundation
+
 struct DefaultSetting {
     static let years: [Int] = Array(2020...2100)
     static let months: [Int] = Array(1...12)
+    static let firstDate: Date = Calendar.current.date(year: 2020, month: 1)!
+    static let lastDate: Date = Calendar.current.date(year: 2101, month: 1)!
 }
