e09fce22548608950c19fafc42ff34ddc9369d38
CoCoral73 <garden0073@gmail.com>
2026-01-14 13:51:47 +0900

    feat: connect IBOutlets for TableViewCell

---
+++ AccountBook/Views/Cells/SearchTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SearchTableViewCell.swift
+//  AccountBook
+//
+//  Created by 김정원 on 1/14/26.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var assetLabel: UILabel!
+    
+}
