cb4afebed7be7cf53d92cae974dd0f851357729c
CoCoral73 <garden0073@gmail.com>
2026-01-23 14:17:06 +0900

    feat: bind ViewModel for implement closure

--- AccountBook/Views/CalendarView/SearchViewController.swift
+++ AccountBook/Views/CalendarView/SearchViewController.swift
@@ -86,6 +86,11 @@ class SearchViewController: UIViewController, ThemeApplicable {
             filterTableView.reloadData()
             showPopUp()
         }
+        
+        viewModel.onRequestReloadData = { [weak self] in
+            guard let self = self else { return }
+            self.tableView.reloadData()
+        }
     }
     
     func configureNavigationBar() {
