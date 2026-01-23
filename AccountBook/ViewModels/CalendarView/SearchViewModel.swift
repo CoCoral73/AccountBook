fe5ac21047cfd0b1e3c01a3a0000dc88211ea0c0
CoCoral73 <garden0073@gmail.com>
2026-01-23 14:16:19 +0900

    feat: call sortFilteredTransaction method in handling sortbutton

--- AccountBook/ViewModels/CalendarView/SearchViewModel.swift
+++ AccountBook/ViewModels/CalendarView/SearchViewModel.swift
@@ -184,6 +184,9 @@ class SearchViewModel {
     
     func handleSortButton(_ tag: Int) {
         sortCriteria = tag
+        sortFilteredTransaction()
+    }
+    
     private func filterTransaction() {
         filteredTxs = allTxs.filter { tx in
             if let keyword = self.keyword, !keyword.isEmpty {
